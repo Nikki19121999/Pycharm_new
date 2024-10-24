e0b6a2f369bbb18e49f5e26420c41e64805bfdc0
KorrakutiNikhilKumar <korrakuti.nikhilkumar@diggibyte.com>
2024-10-24 17:02:34 +0530

    24-10-2024

--- Pyspark_dataframe.py
+++ Pyspark_dataframe.py
@@ -1,14 +1,14 @@
 from pyspark.sql import SparkSession
-
-
+# creating the spark session
 spark = SparkSession.builder.appName("Create DataFrame Example").getOrCreate()
-
+#data
 data = [
     (1, "Mahesh", 29),
     (2, "Rajesh", 31),
     (3, "Suresh", 25)
 ]
+# defining the schema
 columns = ["id", "name", "age"]
 df = spark.createDataFrame(data, schema=columns)
-df.show()
-spark.stop()
+df.show() # display the Dataframe
+spark.stop() # Stop the sparksession
--- read_textfile_pyspark.py
+++ read_textfile_pyspark.py
@@ -1,16 +1,24 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col,desc
+
 #creating a spark session
 spark = SparkSession.builder.appName("WordCountExample").getOrCreate()
+
 # reading the text file from the local
 text_file = spark.read.text("C:\\Users\\korrakutinikhilkumar\\Downloads\\sample1.txt")
+
 # splitting the sentence into words by using space
 words = text_file.selectExpr("explode(split(value, ' ')) as word") #using the explode function to convert list of words into rows
 
 word_counts = words.groupBy("word").count() #grouping by the word and counting the words
+
 word_counts_sorted = word_counts.orderBy(col("count").desc()) #descending order
+
 word_counts_sorted.show() # Display the dataframe
+
 rdd = word_counts_sorted.rdd #converting df to rdd
+
 print(rdd.collect()) #printing the rdd
+
 spark.stop()
 
