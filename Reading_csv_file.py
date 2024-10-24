0fdba5e659318f5ea999cc14a7b1c996e71c31d2
KorrakutiNikhilKumar <korrakuti.nikhilkumar@diggibyte.com>
2024-10-24 17:11:05 +0530

    24-10-2024

--- Reading_csv_file.py
+++ Reading_csv_file.py
@@ -1,9 +1,8 @@
-from Pyspark_dataframe import spark
-
+from pyspark.sql import SparkSession
+# creating the spark session
+spark = SparkSession.builder.appName("Create DataFrame reading csv file").getOrCreate()
 df = spark.read.csv("C:\\Users\\korrakutinikhilkumar\\Downloads\\annual-enterprise-survey-2023-financial-year-provisional.csv", header=True, inferSchema=True)
 
-
+df.show()
 rdd = df.rdd
-
-
 print(rdd.collect())
