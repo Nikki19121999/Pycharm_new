c06a3eb51cfdb7b2fc7c73e7c03138d4228db21f
KorrakutiNikhilKumar <korrakuti.nikhilkumar@diggibyte.com>
2024-10-25 16:36:29 +0530

    25-10-2024

--- reading_difffile.py
+++ reading_difffile.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType,StructField,StringType,IntegerType,FloatType
 spark = SparkSession.builder.appName("Read_file").getOrCreate()
+
 json_schema = StructType([
     StructField("name", StringType(), True),
     StructField("age", IntegerType(), True),
@@ -9,6 +10,7 @@ json_schema = StructType([
 
 df_json = spark.read.schema(json_schema).json("C:/Users/KorrakutiNikhilKumar/Downloads/Json.json")
 df_json.show()
+
 df_csv = spark.read.csv("C:\\Users\\korrakutinikhilkumar\\Downloads\\annual-enterprise-survey-2023-financial-year-provisional.csv", header=True, inferSchema=True)
 df_csv.show()
 
